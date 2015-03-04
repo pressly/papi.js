6f6969fb77bc8d788756c219850b47587c3ba6ba
averted <vitiuk.alex@gmail.com>
2015-03-04 18:33:54 -0500

    more embeds work

--- example/embed/src/app/app.js
+++ example/embed/src/app/app.js
@@ -1,21 +1,27 @@
 import Papi from 'papi';
 import React from 'react';
 import Reflux from 'reflux';
+import moment from 'moment';
 
 /**
  * Pressly Embed.
  */
 function PresslyEmbed(options) {
+  this.$embed = null;
+  this.container = '.pressly-embed-container';
+
   this.defaults = {
-    items: 4,
-    width: 400,
-    height: 250,
+    items: 5,
+    width: 250,
+    height: 200,
+    ewidth: '100%',
   };
 
   this.settings = {
     items:  options.items  || this.defaults.items,
     width:  options.width  || this.defaults.width,
     height: options.height || this.defaults.height,
+    ewidth: options.ewidth || this.defaults.ewidth,
   };
 };
 
@@ -25,30 +31,69 @@ function PresslyEmbed(options) {
 PresslyEmbed.prototype.init = function() {
   var self = this;
 
-  $('.owl-carousel').owlCarousel({
-    items: 1,
+  // update container width
+  $(this.container).css('width', this.settings.ewidth);
+
+  // build carousel
+  this.$embed = $('.pressly-embed').owlCarousel({
+    items: self.settings.items,
     loop: true,
     dots: false,
     nav: true,
     navText: [ '<', '>' ],
     margin: 10,
     autoplay: true,
-    stagePadding: 10,
     lazyLoad: true,
     autoplaySpeed: 500,
     autoplayTimeout: 2000,
     autoplayHoverPause: true,
     itemElement: 'div',
-    responsive: {
-      500:  { items: self.settings.items - 3 || 1 },
-      700:  { items: self.settings.items - 2 || 1 },
-      900:  { items: self.settings.items - 1 },
-      1300: { items: self.settings.items },
-    }
+    //responsive: self.calcResPoints()
   });
-}
 
-// get query params
+  // re-fit assets into container
+  self.refitAssets();
+  $(window).on('resize', debounce(function() { self.refitAssets(); }, 200));
+};
+
+/**
+ * Refit assets to screen.
+ */
+PresslyEmbed.prototype.refitAssets = function() {
+  var containerRatio = Math.floor($(this.container).width() / this.settings.height);
+
+  if (this.settings.items >= containerRatio) {
+    this.$embed.data('owlCarousel').options.items  = containerRatio;
+    this.$embed.data('owlCarousel').settings.items = containerRatio;
+    this.$embed.data('owlCarousel').refresh();
+  }
+};
+
+/**
+ * Calculate responsive points (wip).
+ */
+PresslyEmbed.prototype.calcResPoints = function() {
+  var containerRatio = Math.floor($(this.container).width() / this.settings.height);
+
+  var items = {
+    '1000': this.settings.items <= (containerRatio)     ? this.settings.items : containerRatio,
+    '800' : this.settings.items <= (containerRatio - 1) ? this.settings.items : containerRatio - 1,
+    '600' : this.settings.items <= (containerRatio - 2) ? this.settings.items : containerRatio - 2,
+    '400' : this.settings.items <= (containerRatio - 3) ? this.settings.items : containerRatio - 3,
+    '0'   : 1,
+  };
+
+  return {
+    1000: { items: items['1000'] > 1 ? items['1000'] : 1 },
+    800:  { items: items['800']  > 1 ? items['800']  : 1 },
+    600:  { items: items['600']  > 1 ? items['600']  : 1 },
+    400:  { items: items['400']  > 1 ? items['400']  : 1 },
+    0:    { items: items['0'] },
+  };
+};
+
+
+// query params
 var queryParams = (function(qs) {
   qs = qs.split("+").join(" ");
 
@@ -62,6 +107,22 @@ var queryParams = (function(qs) {
 
   return params;
 })(document.location.search);
+
+// debounce
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    clearTimeout(timeout);
+
+    timeout = setTimeout(function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    }, wait);
+
+    if (immediate && !timeout) func.apply(context, args);
+   };
+};
 
 // initialize new Embed instance
 var presslyEmbed = new PresslyEmbed(queryParams);
@@ -88,6 +149,7 @@ var embedStore = Reflux.createStore({
 
       Papi.loadAssets(hubId).end(function(assets) {
         embedActions.loadAssetsSuccess(assets.body);
+        console.log(assets.body);
       });
     });
   },
@@ -142,7 +204,7 @@ var PresslyEmbedComponent = React.createClass({
         var d       = new Date(item.created_at)
           , width   = presslyEmbed.settings.width
           , height  = presslyEmbed.settings.height
-          , moment  = d.getDay() + '/' + d.getMonth() + '/' + d.getFullYear();
+          , timeAgo = moment(d).fromNow(true) + ' ago';
 
         var imageStyle = {
           backgroundImage: 'url(' + Papi.imgry(item.title_image.url, width, height) + ')',
@@ -155,7 +217,7 @@ var PresslyEmbedComponent = React.createClass({
             <div className="asset-contents">
               <div className="image" style={ imageStyle } />
               <div className="content">
-                <div className="byline">{ item.author.name ? item.author.name + ' - ' : '' }{ moment }</div>
+                <div className="byline">{ item.author.name ? item.author.name + ' - ' : '' }{ timeAgo }</div>
                 <h1 className="title"><a href={ item.source.url } target="_blank" dangerouslySetInnerHTML={{ __html: item.title }}></a></h1>
               </div>
             </div>
@@ -166,7 +228,9 @@ var PresslyEmbedComponent = React.createClass({
     }
 
     return (
-      <ul className="pressly-embed owl-carousel">{ assets }</ul>
+      <div className="pressly-embed-container">
+        <ul className="pressly-embed owl-carousel">{ assets }</ul>
+      </div>
     );
   }
 });
