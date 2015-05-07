224d35da09fab2f67a1960c8ceb5c028e362d2d8
averted <vitiuk.alex@gmail.com>
2015-05-07 14:12:32 -0400

    More work - added Resources for model definitions (wip)

--- papi.js
+++ papi.js
@@ -21,8 +21,7 @@ var alexJwtToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNTRmMGR
 
 
 /**
- * Search Provider
- *
+ * Search Provider.
  * Main wrapper for PAPI Search.
  */
 function SearchProvider(network) {
@@ -31,112 +30,238 @@ function SearchProvider(network) {
   this.network = network;
 };
 
-/**
- * Search:
- *    GET /network/:network/search?q=X
- *    GET /network/:network/user?username=X
- *
- * @param {String} q - Search query
- */
-SearchProvider.prototype.search = function(q) {
-  if (!q) throw new Error('PAPI: Search Query is not defined')
-  if (!Security.isSecurityenticated()) throw new Error('PAPI: unauthorized')
+SearchProvider.prototype = {
+  constructor: SearchProvider,
 
-  var userSearch = (q[0] == '@')
+  /**
+   * Search:
+   *    GET /network/:network/search?q=X
+   *    GET /network/:network/user?username=X
+   *
+   * @param {String} q - Search query
+   */
+  search: function(q) {
+    if (!q) throw new Error('PAPI: Search Query is not defined')
 
-  if (userSearch) {
+    var userSearch = (q[0] == '@')
+
+    if (userSearch) {
+      return request
+        .get(Papi.host + '/network/' + this.network + '/user')
+        .query({ username: q })
+        .query({ jwt: Papi.auth.jwt });
+    }
+
     return request
-      .get(Papi.host + '/network/' + this.network + '/user')
-      .query({ username: q })
+      .get(Papi.host + '/network/' + this.network + '/search')
+      .query({ q: q })
+      .query({ jwt: Papi.auth.jwt });
+  },
+
+  /**
+   * Profile:
+   *    GET /:network/profile/:cred
+   *
+   * @param {Object} cred - Papi credential
+   */
+  profile: function(cred) {
+    if (!cred) throw new Error('PAPI: Credentials were not specified');
+
+    return request
+      .get(Papi.host + '/network/' + this.network + '/profile/' + cred[0].id)
       .query({ jwt: Papi.auth.jwt });
   }
-
-  return request
-    .get(Papi.host + '/network/' + this.network + '/search')
-    .query({ q: q })
-    .query({ jwt: Papi.auth.jwt });
-};
-
-/**
- * Profile:
- *    GET /:network/profile/:cred
- *
- * @param {Object} cred - Papi credential
- */
-SearchProvider.prototype.profile = function(cred) {
-  if (!cred) throw new Error('PAPI: Credentials were not specified');
-
-  return request
-    .get(Papi.host + '/network/' + this.network + '/profile/' + cred[0].id)
-    .query({ jwt: Papi.auth.jwt });
 };
 
 
 /**
- * Security
+ * Security.
  */
 function Security() { };
 
-/**
- * Login:
- *    POST /login
- *
- * @param {Object} creds - Login credentials
- */
-Security.prototype.login = function(creds) {
-  if (!creds) throw new Error('PAPI Security: Unathorized Login - Missing Login Credentials')
-  if (!creds.email) throw new Error('PAPI Security: Unathorized Login - Missing Login Email')
-  if (!creds.password) throw new Error('PAPI Security: Unathorized Login - Missing Login Password')
+Security.prototype = {
+  constructor: Security,
 
-  return $q.promise(function(resolve, reject) {
-    request
-      .post(Papi.host + '/login')
-      .send({ email: creds.email, password: creds.password })
-      .end(function(err, res) {
-        if (res.status == 200) resolve(res.body);
-      });
-  });
+  /**
+   * Login:
+   *    POST /login
+   *
+   * @param {Object} creds - Login credentials
+   */
+  login: function(creds) {
+    if (!creds) throw new Error('PAPI Security: Unathorized Login - Missing Login Credentials')
+    if (!creds.email) throw new Error('PAPI Security: Unathorized Login - Missing Login Email')
+    if (!creds.password) throw new Error('PAPI Security: Unathorized Login - Missing Login Password')
+
+    return $q.promise(function(resolve, reject) {
+      request
+        .post(Papi.host + '/login')
+        .send({ email: creds.email, password: creds.password })
+        .end(function(err, res) {
+          if (res.status == 200) resolve(res.body);
+        });
+    });
+  },
+
+  /**
+   * Logout:
+   *    GET /login
+   *
+   * @param {String} jwt - Jwt token
+   */
+  logout: function(jwt) {
+    if (!jwt) throw new Error('PAPI Security: Unathorized')
+
+    return $q.promise(function(resolve, reject) {
+      request
+        .get(Papi.host + '/auth/logout')
+        .query({ jwt: jwt })
+        .end(function(err, res) {
+          if (res.status == 200) resolve(res.body);
+        });
+    });
+  },
+
+  /**
+   * Session:
+   *    GET /auth/session
+   *
+   * @param {String} jwt - Jwt token
+   */
+  session: function(jwt) {
+    if (!jwt) throw new Error('PAPI Security: Unathorized')
+
+    return $q.promise(function(resolve, reject) {
+      request
+        .get(Papi.host + '/auth/session')
+        .query({ jwt: jwt })
+        .end(function(err, res) {
+          if (res.status == 200) resolve(res.body);
+        });
+    });
+  }
 };
 
 /**
- * Logout:
- *    GET /login
- *
- * @param {String} jwt - Jwt token
+ * Resource.
  */
-Security.prototype.logout = function(jwt) {
-  if (!jwt) throw new Error('PAPI Security: Unathorized')
+function Resource() {
+  this.params = null;
+};
 
-  return $q.promise(function(resolve, reject) {
-    request
-      .get(Papi.host + '/auth/logout')
-      .query({ jwt: jwt })
-      .end(function(err, res) {
-        if (res.status == 200) resolve(res.body);
-      });
-  });
+Resource.prototype = {
+  constructor: Resource,
+  reRouteParams: /:[^\/:]+/gi,
+
+  $all: function(params) {
+    var self = this, params = params || {};
+    this.params = _.extend(params, { jwt: Papi.auth.jwt });
+
+    return $q.promise(function(resolve, reject) {
+      request
+        .get(Papi.host + self.buildRoute())
+        .query(self.params)
+        .end(function(err, res) {
+          if (res.status == 200) resolve(res.body);
+        });
+    });
+  },
+
+  $find: function(id) {
+    var self = this;
+    this.params = _.extend({ id: id }, { jwt: Papi.auth.jwt });
+
+    return $q.promise(function(resolve, reject) {
+      request
+        .get(Papi.host + self.buildRoute())
+        .query(self.params)
+        .end(function(err, res) {
+          if (res.status == 200) resolve(res.body);
+        });
+    });
+  },
+
+  $save: function(resource) {
+    var self = this;
+    this.params = { id: resource.id };
+
+    return $q.promise(function(resolve, reject) {
+      request
+        .put(Papi.host + self.buildRoute())
+        .query({ jwt: Papi.auth.jwt })
+        .send(resource)
+        .end(function(err, res) {
+          if (res.status == 200) resolve(res.body);
+        });
+    });
+  },
+
+  $saveAll: function(resources) {
+
+  },
+
+  $update: function(resource) {
+
+  },
+
+  parseRouteParams: function() {
+    return _.map(this.route.match(this.reRouteParams), function(param) {
+      return param.substr(1, param.length);
+    });
+  },
+
+  buildRoute: function() {
+    var self          = this
+      , finalRoute    = ''
+      , paramsArray   = this.parseRouteParams()
+      , routeSegments = this.route.split(':');
+
+    // map params to route segments
+    _.each(routeSegments, function(segment, index) {
+      if (_.contains(paramsArray, segment)) {
+        routeSegments[index] = self.params[segment];
+
+        delete self.params[segment]; // remove from global params
+      }
+    });
+
+    // concat segments
+    finalRoute = routeSegments.join('');
+
+    // strip last char if forward slash
+    if (finalRoute.substr(-1) === '/')
+      finalRoute = finalRoute.substr(0, finalRoute.length - 1)
+
+    return finalRoute;
+  }
 };
 
 /**
- * Session:
- *    GET /auth/session
- *
- * @param {String} jwt - Jwt token
+ * Hub Resource.
  */
-Security.prototype.session = function(jwt) {
-  if (!jwt) throw new Error('PAPI Security: Unathorized')
+function HubResource() {
+  this.route = '/hubs/:id:';
+}
+HubResource.prototype = new Resource();
+HubResource.prototype.constructor = HubResource;
 
-  return $q.promise(function(resolve, reject) {
-    request
-      .get(Papi.host + '/auth/session')
-      .query({ jwt: jwt })
-      .end(function(err, res) {
-        if (res.status == 200) resolve(res.body);
-      });
-  });
-};
+/**
+ * Stream Asset Resource.
+ */
+function StreamAssetResource() {
+  this.route = '/hubs/:hubId:/stream/:assetId:';
+}
+StreamAssetResource.prototype = new Resource();
+StreamAssetResource.prototype.constructor = StreamAssetResource;
 
-
+/**
+ * User Resource.
+ */
+function UserResource() {
+  this.route = '/users/:id:';
+}
+UserResource.prototype = new Resource();
+UserResource.prototype.constructor = UserResource;
 
 
 
@@ -273,19 +398,47 @@ Papi.auth = {
   },
 };
 
+Papi.hubs  = new HubResource();
+Papi.users = new UserResource();
+Papi.hubs.stream = new StreamAssetResource();
+
 module.exports = Papi;
 
+//
+//
+// TESTING
 Papi.auth.login({ email: 'alex.vitiuk@pressly.com', password: 'betame' }).then(function(res) {
-  console.log('LOGIN SUCCESS', res);
-
-  Papi.auth.requestCurrentUser().then(function(res) {
-    console.log('REQUEST CURR USERS SUCCESS', res)
-  });
-
   /* LOGOUT
   Papi.auth.logout().then(function(res) {
     console.log('LOGOUT SUCCESS', res);
   });
   */
-});
 
+  // FIND ONE HUB
+  Papi.hubs.$find('5540ed5d468bb00001000042').then(function(hub) {
+    console.log("FIND HUB", hub);
+
+    // HUB STREAM ASSETS
+    Papi.hubs.stream.$all({ hubId: '5540ed5d468bb00001000042' }).then(function(assets) {
+      console.log('STREAM ASSETS', assets);
+    });
+
+    // SAVE HUB
+    Papi.hubs.$save(hub).then(function(savedHub) {
+      console.log("SAVED HUB", savedHub);
+    });
+  });
+
+  /* ALL HUBS
+  Papi.hubs.$all().then(function(hubs) {
+    console.log("ALL HUBS", hubs);
+  });
+  */
+
+
+  /* ALL USERS
+  Papi.users.$all().then(function(users) {
+    console.log("ALL USERS", users);
+  });
+  */
+});
