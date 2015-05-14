720e998f076d9b70dbc2f2644c5a55704c5da270
averted <vitiuk.alex@gmail.com>
2015-05-14 17:43:04 -0400

    Better tests

--- test/hubs/test-hubs.js
+++ test/hubs/test-hubs.js
@@ -1,11 +1,19 @@
 'use strict';
 
 import nock from 'nock';
+import should from 'should';
 import Papi from '../../src';
 
 const api = new Papi('https://beta-api.pressly.com');
 
 // mock requests
+const mockHub = {
+  id: '5540ed5d468bb00001000042',
+  uid: 'hacktheplanet',
+  name: 'HackThePlanel',
+  account_id: 'peter'
+};
+
 nock(api.session.domain)
   .post('/login', {
     email: 'alex.vitiuk@pressly.com',
@@ -14,13 +22,11 @@ nock(api.session.domain)
     jwt: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNTRmMGRiNzMwOGFmYTEyYjUzNjIwNTg4In0.CvXGDKAJYZkoH3nnEirtlGlwRzErv1ANOJ-dVkUAnjo#_login_post'
   });
 
-// mock authorization requests
 nock(api.session.domain, { reqheaders: { 'Authorization': `Bearer ${api.session.jwt}` } })
-  .get('/hubs').reply(200, {
-    status: 200,
-    body: { }
-  });
+  .get('/hubs').reply(200)
+  .get(`/hubs/${mockHub.id}`).reply(200, mockHub);
 
+// tests
 describe('Testing Hubs API - $all', function () {
   it('should return all hubs', function (done) {
     api.auth.login('alex.vitiuk@pressly.com', 'betame').then(() => {
@@ -31,6 +37,23 @@ describe('Testing Hubs API - $all', function () {
 
         done();
       });
+    }).catch((err) => {
+      done(err);
+    });
+  });
+
+  it('should return one hub', function (done) {
+    api.hubs.$find(mockHub.id).then((res) => {
+      if (res.status != 200) {
+        throw new Error('Something went wrong');
+      }
+
+      res.body.id.should.be.exactly(mockHub.id);
+      res.body.uid.should.not.be.empty;
+      res.body.name.should.not.be.empty;
+      res.body.account_id.should.not.be.empty;
+
+      done();
     }).catch((err) => {
       done(err);
     });
--- test/users/test-users.js
+++ test/users/test-users.js
@@ -1,10 +1,19 @@
 'use strict';
 
-import nock from 'nock';
 import Papi from '../../src';
+import nock from 'nock';
+import should from 'should';
+
 const api = new Papi('https://beta-api.pressly.com');
 
-// mock requests
+// mock
+const mockUser = {
+  id: '54f0db7308afa12b53620588',
+  email: 'alex.vitiuk@pressly.com',
+  username: 'alex',
+  account_id: '54f0db7308afa12b53620587'
+};
+
 nock(api.session.domain)
   .post('/login', {
     email: 'alex.vitiuk@pressly.com',
@@ -13,17 +22,11 @@ nock(api.session.domain)
     jwt: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNTRmMGRiNzMwOGFmYTEyYjUzNjIwNTg4In0.CvXGDKAJYZkoH3nnEirtlGlwRzErv1ANOJ-dVkUAnjo#_login_post'
   });
 
-// mock authorization requests
 nock(api.session.domain, { reqheaders: { 'Authorization': `Bearer ${api.session.jwt}` } })
-  .get('/users').reply(200, {
-    status: 200,
-    body: { }
-  })
-  .get('/users/54f0db7308afa12b53620588').reply(200, {
-    status: 200,
-    body: { }
-  });
+  .get('/users').reply(200, { status: 200, body: { } })
+  .get(`/users/${mockUser.id}`).reply(200, mockUser);
 
+// tests
 describe('Testing Users API - $all', function () {
   it('should return all users', function (done) {
     api.auth.login('alex.vitiuk@pressly.com', 'betame').then(() => {
@@ -40,12 +43,15 @@ describe('Testing Users API - $all', function () {
   });
 
   it('should return one user', function (done) {
-    let userId = '54f0db7308afa12b53620588';
-
-    api.users.$find(userId).then((res) => {
+    api.users.$find(mockUser.id).then((res) => {
       if (res.status != 200) {
         throw new Error('Something went wrong');
       }
+
+      res.body.id.should.be.exactly(mockUser.id);
+      res.body.email.should.be.exactly(mockUser.email);
+      res.body.username.should.not.be.empty;
+      res.body.account_id.should.not.be.empty;
 
       done();
     }).catch((err) => {
