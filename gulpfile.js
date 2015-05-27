ba5e05900e0a3e77993bdb0508c3a39756192cff
Corban Brook <corbanbrook@gmail.com>
2015-05-27 16:32:55 -0400

    Added minification to papi.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 var gulp       = require('gulp')
   , connect    = require('gulp-connect')
+  , babel      = require('gulp-babel')
+  , size       = require('gulp-size')
+  , uglify     = require('gulp-uglify')
+  , rename     = require('gulp-rename')
   , browserify = require('browserify')
   , source     = require('vinyl-source-stream')
   , babelify   = require('babelify')
-  , babel      = require('gulp-babel');
+;
 
 
 
@@ -22,7 +26,17 @@ gulp.task('build', ['build:es6'], function () {
     .pipe(connect.reload());
 });
 
-gulp.task('dist', ['build'], function() {
+gulp.task('dist:minify', ['build'], function() {
+  return gulp.src('build/papi.js')
+  .pipe(size({showFiles: true}))
+  .pipe(rename('papi.min.js'))
+  .pipe(uglify({mangle: true, preserveComments: 'some'}))
+  .pipe(size({showFiles: true}))
+  .pipe(size({showFiles: true, gzip: true}))
+  .pipe(gulp.dest('dist'));
+});
+
+gulp.task('dist', ['build', 'dist:minify'], function() {
   return gulp.src('build/**/*')
   .pipe(gulp.dest('dist'));
 });
