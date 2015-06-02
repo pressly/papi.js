a81242ec143e14a3f1cd1a8ba70f3d692ef556cb
Corban Brook <corbanbrook@gmail.com>
2015-06-02 11:30:24 -0400

    Base methods in model.

--- src/model.js
+++ src/model.js
@@ -3,7 +3,25 @@
 import _ from 'lodash';
 
 export default class Model {
-  constructor(data) {
+  constructor(data, options = {}) {
     _.extend(this, data);
+
+    if (!options.persisted) {
+      this.$newRecord = true;
+    }
+  }
+
+  $delete() {
+    return this.$resource().request('delete');
+  }
+
+  $save() {
+    var method = this.$newRecord ? 'post' : 'put';
+
+    return this.$resource().request({method: method, data: this }).then((res) => {
+      delete this.$newRecord;
+
+      return _.extend(this, res);
+    });
   }
 }
