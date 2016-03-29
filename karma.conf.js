b5448d96ab95738cba0f181b543144a9a3149cb5
Corban Brook <corbanbrook@gmail.com>
2016-03-29 11:24:05 -0400

    Adding Object assign support for phantomjs.

--- karma.conf.js
+++ karma.conf.js
@@ -21,6 +21,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+      './node_modules/phantomjs-polyfill-object-assign/object-assign-polyfill.js',
       './node_modules/phantomjs-polyfill/bind-polyfill.js',
       'test/**/*.js'
     ],
